8d8a1f74dbb0e31be912aab5788c9ca14e21cba8
이진영 <jakerhee321@gmail.com>
2022-08-29 13:06:51 +0900

    Functions and Player Turn

--- Connect_Four.py
+++ Connect_Four.py
@@ -2,3 +2,23 @@ import numpy as np
 import math
 import pygame
 import sys
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Player 1 Input
+    if turn == 0:
+        selection = int(input("Player 1 Make your Selection (0-6)"))
+
+    # Player 2 Input
+    else:
+        selection = int(input("Player 2 Make your Selection (0-6)"))
+
+    turn += 1
+    turn = turn % 2 
