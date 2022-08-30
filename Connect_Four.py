f072396587e7959413f16349f30f5a8172ceda5f
이진영 <jakerhee321@gmail.com>
2022-08-30 19:13:16 +0900

    Drawing board after action

--- Connect_Four.py
+++ Connect_Four.py
@@ -3,12 +3,39 @@ import math
 import pygame
 import sys
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
 def create_board():
     board = np.zeros((6,7))
     return board
 
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board():
+    pass
+
+def winning_move():
+    pass
+
+def draw_board(board):
+    print(np.flip(board, 0))
+    
+
+
+
 board = create_board()
 game_over = False
+draw_board(board)
 turn = 0
 
 while not game_over:
@@ -16,9 +43,19 @@ while not game_over:
     if turn == 0:
         selection = int(input("Player 1 Make your Selection (0-6)"))
 
+        if is_valid_location(board, selection):
+            row = get_next_open_row(board, selection)
+            drop_piece(board, row, selection, 1)
+    
     # Player 2 Input
     else:
         selection = int(input("Player 2 Make your Selection (0-6)"))
+
+        if is_valid_location(board, selection):
+            row = get_next_open_row(board, selection)
+            drop_piece(board, row, selection, 2)
+            
+    draw_board(board)
 
     turn += 1
     turn = turn % 2 
