11c2d2fcefb53eaa2dc83d2cccbc2c4bb4aa1698
이진영 <jakerhee321@gmail.com>
2022-08-31 14:56:07 +0900

    Winning conditions #1

--- Connect_Four.py
+++ Connect_Four.py
@@ -7,35 +7,48 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
 
-def print_board():
-    pass
+def print_board(board):
+    print(np.flip(board, 0))
 
-def winning_move():
-    pass
+def winning_move(board, piece):
+    # Horizontal check
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Vertical check
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True 
+    
+    # Diagonal check
+    # NE direction
+
+
+    # NW direction
 
 def draw_board(board):
-    print(np.flip(board, 0))
-    
-
-
+    pass
 
 board = create_board()
 game_over = False
-draw_board(board)
+print_board(board)
 turn = 0
 
 while not game_over:
@@ -46,6 +59,10 @@ while not game_over:
         if is_valid_location(board, selection):
             row = get_next_open_row(board, selection)
             drop_piece(board, row, selection, 1)
+
+            if winning_move(board, 1):
+                print("PLAYER 1 Wins")
+                game_over = True
     
     # Player 2 Input
     else:
@@ -54,8 +71,12 @@ while not game_over:
         if is_valid_location(board, selection):
             row = get_next_open_row(board, selection)
             drop_piece(board, row, selection, 2)
+
+            if winning_move(board, 2):
+                print("PLAYER 2 Wins")
+                game_over = True
             
-    draw_board(board)
+    print_board(board)
 
     turn += 1
     turn = turn % 2 
