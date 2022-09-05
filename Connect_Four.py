5dbd9cc2de7ed90108ff4cb23954cd1abc03450c
이진영 <jakerhee321@gmail.com>
2022-09-05 23:38:04 +0900

    Visualize Player's turn

--- Connect_Four.py
+++ Connect_Four.py
@@ -90,6 +90,16 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
         
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+        
+        pygame.display.update()
+        
         if event.type == pygame.MOUSEBUTTONDOWN:
             # # Player 1 Input
             if turn == 0:
